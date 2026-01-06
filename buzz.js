91daab0e00a45279556bd5f0eedc3419bdc5c446
kanoloa <kanoloa@gmail.com>
2026-01-06 11:27:48 +0900

    #1023095, beep function added.

---
+++ buzz.js
@@ -0,0 +1,10 @@
+const BEEP = 9;
+const beepsound = 'https://mazda.com/ivi/sounds/beep.mp3';
+
+function main(signal) {
+    if (signall === BEEP) {
+        sound.play(beepsound);
+    }
+}
+
+main();

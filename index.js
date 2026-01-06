f5d6651fc41645ee184f2e25d8805b1a508a96fe
kanoloa <kanoloa@gmail.com>
2026-01-06 09:40:54 +0900

    First commit

---
+++ index.js
@@ -0,0 +1,41 @@
+/*
+ * This program is for demonstration. Do not use in any production environment.
+ * MAZDA INNOVATORS PROGRAM, 2025 - 2026
+ * Created by kanoloa@mac.com
+ */
+
+const RPC_STATE_INITIATE = 0;
+const RPC_STATE_MOVE = 1;
+const RPC_STATE_HALT = 2;
+const RPC_STATE_ABORT = 3;
+const RPC_STATE_COMPLETE = 9;
+const RPC_VEHICLE_DIRECTION_FORWARD = 1;
+const RPC_VEHICLE_DIRECTION_REVERSE = 2;
+
+let receiveData;
+let sendData;
+
+function init() {
+    console.log("status initiating.");
+    return 1;
+}
+
+function onMove() {
+    console.log("vehicle is now on move...")
+    return 2;
+}
+
+function main() {
+    console.log("Program starts.");
+
+    let result;
+    switch(receiveData) {
+        case RPC_STATE_INITIATE:
+            result = init();
+            break;
+        case RPC_STATE_MOVE:
+            result = onMove();
+    }
+
+    console.log("Program finished successfully.")
+}
